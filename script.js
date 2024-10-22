b4619662d3366d453b9dccb913fe9af7fe1249ab
Egor <cefranovegor@gmail.com>
2024-10-22 18:59:04 +0300

    Добавленны изминения

---
+++ script.js
@@ -0,0 +1,90 @@
+//   let speed = 20 
+//     if (speed<30){
+//         conole.log('ваша скорость медленная' + speed);
+//  } else {
+//        console.log('ваша скорость быстрая' + speed);
+//  }
+
+// оператор if else как правило используется для конкретного значения
+
+// let symbol = 3
+//  switch (symbol) {
+//      case 1:
+//         console.log('!')
+//         break;
+//     case 2:
+//         console.log('@')
+//             break;
+//      case 3:
+//        console.log('#')
+//           break;
+//      case 4:
+//           console.log('$')
+//           break;
+//       case 5:
+//           console.log('%')
+//               break;
+//        case 6:
+//          console.log('^')
+//             break;
+  
+//          default:   
+//           console.log ('значение не найдено')
+//           break;
+//  }
+
+//  let day = +prompt('введите день недели')
+//  switch (day){
+//     case 1:
+//         console.log('понедельник')
+//        break;
+//      case 2:
+//         console.log('вторник')
+//        break;
+//     case 3:
+//         console.log('среда')
+//        break;
+//      case 4:
+//         console.log('четверг')
+//        break;
+//        case 5:
+//         console.log('пятница')
+//        break;      
+//        case 6:
+//         console.log('суббота')
+//        break;
+//        case 7:
+//         console.log('воскресенье')
+//        break;
+
+//     default:
+//         console.log('Введите корректное число')
+//         break;
+//  }
+// let min = 1
+// let max = 6
+
+// let accumulator = 0
+// consoleog('значение ДО =' +accumulator)
+
+
+//  for (let i = min; i < max; i++) {
+//     accumulator+=i
+//     console.log (i + 'шаг');
+
+
+//     console.log(accumulator);
+  
+//  }
+
+//  console.log('значение ПОСЛЕ = ' + accumulator);
+
+
+//  let palindrom = '12345'
+//  console.log(palindrom);
+
+//  let checkPalidrom = palindrom.split('')//метод разделает строку по символам
+// .reverse()//метод, переставляющий символы в обратном порядке(тоесть на оборот)
+// .join()//метод соединяет строку по символам
+
+//  console.log(checkPalidrom);
